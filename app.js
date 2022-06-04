9d3e5267cdcbf8cf6afffd088993db295935ff20
Shane <98501191+codebyshane@users.noreply.github.com>
2022-06-03 21:30:56 -0400

    update to latest documents. Images on Standings page converted from JPEG to JPG for faster load times.

---
+++ app.js
@@ -0,0 +1,7 @@
+// Calculating round total
+
+let frontNineTotalStrokes = [holeOne, holeTwo, holeThree, holeFour, holeFive, holeSix, holeSeven, holeEight, holeNine]
+let backNineTotalStrokes = [holeTen, holeEleven, holeTwelve, holeThirteen, holeFourteen, holeFifteen, holeSixteen, holeSeventeen, holeEighteen]
+
+// Each hole is an object
+
