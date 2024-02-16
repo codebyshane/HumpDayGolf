8f8bbd660fc4e0f90482c055b6b366d1ad5a7b97
Shane <98501191+codebyshane@users.noreply.github.com>
2024-02-15 21:37:11 -0500

    update to 2024 dates and info

--- app.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("#my-button").addEventListener("click", showNav);
-
-function showNav() {
-    document.querySelector("#mynav").classList.toggle("show");
-    document.querySelector(".logo").classList.toggle("hideLogo");
-}
